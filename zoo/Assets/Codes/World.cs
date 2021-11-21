e2a45a3061c14d4672a89c57cb8fa88b6cd1cfdd
csehkristofdavid <csehkristofdavid3576@gmail.com>
2021-11-21 10:51:03 +0100

    zoo0.9

---
+++ zoo/Assets/Codes/World.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+	Zoo zoo;
+	
+    // Start is called before the first frame update
+	//világ létrehozása
+    void Start()
+    {
+       zoo = new Zoo(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
